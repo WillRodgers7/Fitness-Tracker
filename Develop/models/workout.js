17fba4a48e386d39577cc5972b514de2e19c68cd
Will Rodgers <WillRodgers.e.s@gmail.com>
2020-10-31 15:02:33 -0700

    new update

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -1,62 +1,56 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-day: {
-    type: Date,
-    default:Date.now,
-},
-exercises:[
-    {
-        type: {
-            type:String,
-            trim:true,
-            required:true
-        },
-        name: {
-            type:String,
-            trim:true,
-            required:true
-        },
-        duration:{
-            type:Number,
-            required:true
-        },
-        weight: {
-            type:Number,
-        },
-        reps: {
-            type:Number,
-        },
-        sets: {
-            type:Number,
-        },
-        distance: {
-            type:Number,
-        }
-    }
-]
-
-},{
-    toJSON: {
-    virtuals:true  
-
-}});
+const workoutSchema = new Schema(
+	{
+		day: {
+			type: Date,
+			default: Date.now,
+		},
+		exercises: [
+			{
+				type: {
+					type: String,
+					trim: true,
+					required: true,
+				},
+				name: {
+					type: String,
+					trim: true,
+					required: true,
+				},
+				duration: {
+					type: Number,
+					required: true,
+				},
+				weight: {
+					type: Number,
+				},
+				reps: {
+					type: Number,
+				},
+				sets: {
+					type: Number,
+				},
+				distance: {
+					type: Number,
+				},
+			},
+		],
+	},
+	{
+		toJSON: {
+			virtuals: true,
+		},
+	}
+);
 // Add more Things
 
 // schema name, define virtual .virtual
 
-
-
-// workoutSchema.virtuals.
-
-// setFullName = function() {
-//     this.fullName = `${this.firstName} ${this.lastName}`;
-
-//     return this.fullName;
-// };
-
-
+workoutSchema.virtual('totalDuration').get(function() {  
+    return this.exercises[duration] + ' ' + this.exercises[weight];
+})
 
 
 
